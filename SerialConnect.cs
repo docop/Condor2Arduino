784a9b2944b88853d3fade0f3126e5f0e17db3b7
Enrico66nl <eric.wijngaarden@gmail.com>
2020-01-09 15:36:48 +0100

    threading works
    Backgroundworker added and tested. it works :)

--- SerialConnect.cs
+++ SerialConnect.cs
@@ -11,26 +11,40 @@ using System.Net.Sockets;
 using System.Globalization;
 using System.IO.Ports; // for the serial port connections
 using System.Threading; // threading
-
+using System.Runtime.InteropServices;
 
 namespace Condor2Arduino
 {
-    public class SerialConnect
+    class SerialConnect
     {
-        public SerialConnect()
-        {
-            
-        UdpClient Ontvanger;
-        IPEndPoint listenEndPoint;
-        byte[] receivedData;
-        int poort;
-        SerialPort port;
-        bool connected = false;
-        bool arduino = false;
-        
-        
-        //
+        public UdpClient Ontvanger;
+        public IPEndPoint listenEndPoint;
+        public byte[] receivedData;
+        public int poort;
+        public SerialPort port;
+        public bool connected = false;
+        public bool arduino = false;
 
+        public void CondorConnect()   
+    {
+            try
+            {
+                if (!connected)
+                {
+                    poort = Convert.ToInt32(textBox1.Text); //converts port string to variable  poort as int32
+                    Ontvanger = new UdpClient(poort); //create new client using port 'poort'
+                    Ontvanger.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 100);
+                    listenEndPoint = new IPEndPoint(IPAddress.Any, poort);
+                    connected = true;
+                }
+                else
+                {
+                    Ontvanger.Close();
+                    connected = false;
+                }
+            }
+            catch (Exception f) { } // no exception catch coded. 
         }
+        
     }
 }
