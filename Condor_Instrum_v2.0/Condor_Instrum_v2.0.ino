539ef17af98184b4aab3ec448acb518b5c17b0fa
Enrico66nl <eric.wijngaarden@gmail.com>
2020-01-18 19:11:54 +0100

    Update Condor_Instrum_v2.0.ino

--- Condor_Instrum_v2.0/Condor_Instrum_v2.0.ino
+++ Condor_Instrum_v2.0/Condor_Instrum_v2.0.ino
@@ -9,7 +9,6 @@
 
 #include <Wire.h> // needed for the liquid crystal display (LCD)
 #include <LiquidCrystal_I2C.h> // LCD display
-#include <TM1638plus.h> // library for the 8 segments display / 8 buttons, 8 led red/green 
 #include <TM1638.h> //older library for the 8 segments display / 8 buttons, 8 led red/green 
 
 #define motorPin1  8      // IN1 on the ULN2003 driver
@@ -22,23 +21,6 @@
 #define  CLOCK_TM 2       // CLK of all boards is on pin 2
 #define  DIO_TM 3         // DIO of all boards is on pin 3
 #define TM_BRT 0x02       // set the brightness of board (0-7)
-
-//*************************************************************************
-TM1638plus tm(STROBE_TM1, CLOCK_TM , DIO_TM); // define object of TM1638
-TM1638plus tm2(STROBE_TM2, CLOCK_TM , DIO_TM); // define object of TM1638
-//*************************************************************************
-/*
-  void reset();
-  void brightness(uint8_t brightness)--> Sets the brightness level on a scale of brightness = 0 to 7.
-  uint8_t readButtons(void)-->Read buttons returns a byte with value of buttons 1-8 b7b6b5b4b3b2b1b0
-  void setLED(uint8_t position, uint8_t value)-->Set an LED, pass it LED position 0-7 and value 0 or 1
-  void displayText(const char *text)-->send Text to Seven segments, passed char array pointer
-  void displayASCII(uint8_t position, uint8_t ascii)
-  void displayASCIIwDot(uint8_t position, uint8_t ascii)
-  void displayHex(uint8_t position, uint8_t hex)
-  void display7Seg(uint8_t position, uint8_t value);
- **************************************************************************
-*/
 
 TM1638 tm3(DIO_TM, CLOCK_TM, STROBE_TM1);
 TM1638 tm4(DIO_TM, CLOCK_TM, STROBE_TM2);
@@ -135,50 +117,18 @@ AccelStepper ACSpeedStepper(AccelStepper::FULL4WIRE, 8, 10, 9, 11); // refer to 
 // *********************************************
 float stpSpeed = 10.50; //how many steps per km/h
 
-int CodeIn;// used in the serial reads
 uint8_t buttons;
 
-String lcdtekst = "Condor2Arduino";
-
+byte bbuttons, oldbuttons, page;
 bool homing = false;
-bool lcdon = true;
+bool lcdon = false;
 bool TM1638on = true;
-
-
-//Vario variables
-double Vario = 0.0;
-double eVario = 0.0;
-double iVario = 0.0;
-double Gforce = 0;
-
-
-String SpeedRead = "";
-String VarioRead = "";
-String eVarioRead = "";
-String iVarioRead = "";
-String AltRead = "";
-String GforceRead = "";
-String CompassRead = "";
-String BankRead = "";
-String PitchRead = "";
-
-int AltOld = 0;
-int AltNew = 0;
-double VarioOld = 0.0;
-double VarioNew = 0.0;
-int SpeedOld = 0;
-int SpeedNew = 0;
-int CompOld = 0;
-int CompNew = 0;
-int Bankint = 0;
-int Pitchint = 0;
-int Compassint = 0;
+bool changed;
 
 int altl, spdl, hdgl, bnkl, pitl, varrl, varel, varil, gfol;
 int alth, spdh, hdgh, bnkh, pith, varrh, vareh, varih, gfoh;
 int alt, spd, hdg, bnk, pit;
 double varr, vare, vari, gfo;
-
 
 
 void setup()
@@ -191,30 +141,32 @@ void setup()
   ACSpeedStepper.setSpeed(400.0); // set it at max
   ACSpeedStepper.setCurrentPosition(0); // this needs to be in a homing routine. For now I assume the current position = 0 km/h
 
-  if (lcdon) {
+  if (lcdon)
+  {
     lcd.init();                  // initialiseer het LCD scherm
     lcd.backlight();             // zet de backlight aan
     lcd.clear();                 // maak leeg
     lcd.setCursor(0, 0);         // cursor linksboven
-    lcd.print(lcdtekst);         // print tekst in var lcdtekst
+  }
+  else
+  { // lcd is off
+    lcd.clear();
+    lcd.noBacklight();
   }
 
   if (TM1638on)
   {
-    /*
-      tm.reset();
-      tm2.reset();
-      tm.brightness(TM_BRT);
-      tm2.brightness(TM_BRT);
-      tm.displayText("Condor 2");
-      tm2.displayText("Arduino");
-    */
     tm3.setupDisplay(true, 2);
     tm4.setupDisplay(true, 2);
     tm3.clearDisplay();
     tm4.clearDisplay();
-    tm3.setDisplayToString("Condor 2", 0, 0);
-    tm4.setDisplayToString("Arduino", 0, 0);
+  }
+  else // its off
+  {
+    tm3.clearDisplay();
+    tm4.clearDisplay();
+    tm3.setupDisplay(false, 2);
+    tm4.setupDisplay(false, 2);
   }
 
   Serial.begin(19200);
@@ -247,6 +199,7 @@ void loop()
         gfol = Serial.read();//serialdata[17]
         gfoh = Serial.read();//serialdata[18]
 
+
         alt = altl << 8 | alth; //decode altitude
         spd = spdl << 8 | spdh; //decode speed (kmph)
         hdg = hdgl << 8 | hdgh; //decode compass
@@ -256,10 +209,23 @@ void loop()
         vare = ((varel << 8 | vareh) / 10.0) - 99.9; //decode elec vario m/s
         vari = ((varil << 8 | varih) / 10.0) - 99.9; //decode integrated vario m/s
         gfo = ((gfol << 8 | gfoh) / 10.0) - 9.9; //decode gforce
+
+        //debug info
+        if (lcdon)
+        {
+          lcd.setCursor(0, 0);
+          //   lcd.print(spd);
+          //   lcd.print(alt);
+          // lcd.print(gfo);
+          // lcd.print(bnk);
+          //  lcd.print(pit); // max4
+          lcd.print(varr);
+        }
+        if (TM1638on)
+          TMD();
       }
     }
   }
-
   if (homing) // used to set the speed to zero position with software.
   {
     ACSpeedStepper.runSpeed();
@@ -267,65 +233,53 @@ void loop()
   else //normal situation.
   {
     // Set the SPEED
-    SpeedNew=spd;
-    ACSpeedStepper.moveTo(SpeedNew * stpSpeed); // where to go
+    ACSpeedStepper.moveTo(spd * stpSpeed); // where to go
     ACSpeedStepper.run(); //go
 
     // Set the VARIO
-    Vario=vari;
-    MyServo.write(Vario * StepVario + 100); //
-
-    if (lcdon)
-    {
-     lcd.setCursor(0, 1);
-     lcd.print(spd);
-     lcd.setCursor(13, 1);
-     lcd.print(alt);
-    }
-
+    MyServo.write(varr * StepVario + 100); //
   }
 }
 
-
-/*
-    case 'A'://Found the second identifier
-      {
-        homing = true;
-        ACSpeedStepper.setSpeed(100);
-        break;
-      }
-    case 'D'://Found the second identifier
-      {
-        homing = false;
-        ACSpeedStepper.setCurrentPosition(0);
-        ACSpeedStepper.setSpeed(400);
-        break;
-*/
-
-
-void LCD_A(String a)
+void TMD()
 {
-  lcd.setCursor(0, 1);
-  lcd.print(a);
-
-}
-void LCD_B(String b)
-{
-  lcd.setCursor(13, 1);
-  lcd.print(b);
-}
-void TMDisplay(String a, String b)
-{
-  tm3.setDisplayToString(b, 0, 4);
-  tm4.setDisplayToString(a, 0, 4);
-}
-void TMDisplay1(String a, String b)
-{
-  tm3.setDisplayToString(a, 0, 0);
-  tm3.setDisplayToString(b, 0, 3);
-}
-void TMDisplay2(String a, String b)
-{
-  tm4.setDisplayToString(a, 0, 0);
-  tm4.setDisplayToString(b, 0, 3);
+  tm3.setDisplayToString("alt", 0, 0);
+  tm4.setDisplayToString("spd", 0, 0);
+  if (spd < 10)
+  {
+    tm4.clearDisplayDigit(5, false);
+    tm4.clearDisplayDigit(6, false);
+    tm4.setDisplayToString(String(spd, DEC), 0, 7);
+  }
+  if (spd >= 10 && spd < 100)
+  {
+    tm4.clearDisplayDigit(5, false);
+    tm4.setDisplayToString(String(spd, DEC), 0, 6);
+  }
+  if (spd > 100)
+  {
+    tm4.setDisplayToString(String(spd, DEC), 0, 5);
+  }
+  if (alt < 10)
+  {
+    tm3.clearDisplayDigit(4, false);
+    tm3.clearDisplayDigit(5, false);
+    tm3.clearDisplayDigit(6, false);
+    tm3.setDisplayToString(String(alt, DEC), 0, 7);
+  }
+  if (alt < 100 && alt >= 10)
+  {
+    tm3.clearDisplayDigit(4, false);
+    tm3.clearDisplayDigit(5, false);
+    tm3.setDisplayToString(String(alt, DEC), 0, 6);
+  }
+  if (alt < 1000 && alt >= 100)
+  {
+    tm3.clearDisplayDigit(4, false);
+    tm3.setDisplayToString(String(alt, DEC), 0, 5);
+  }
+  if (alt < 10000 && alt >= 1000)
+  {
+    tm3.setDisplayToString(String(alt, DEC), 0, 4);
+  }
 }
