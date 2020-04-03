1b2140ce6f7a813552e986ddc1349fb7a444375c
Enrico66nl <eric.wijngaarden@gmail.com>
2020-04-03 17:34:46 +0200

    cleanup

--- Condor_Instrum_v2.0/Condor_Instrum_v2.0.ino
+++ Condor_Instrum_v2.0/Condor_Instrum_v2.0.ino
@@ -1,8 +1,5 @@
 #include <Servo.h> // link to the servo library. 
 #include <Wire.h> // needed for the liquid crystal display (LCD)
-#include <LiquidCrystal_I2C.h> // LCD display
-#include <TM1638.h> // library for the 8 segments display / 8 buttons, 8 led red/green 
-
 #include <Stepper.h>
 
 #define motorPin1  8      // IN1 on the ULN2003 driver
@@ -10,13 +7,6 @@
 #define motorPin3  10     // IN3 on the ULN2003 driver
 #define motorPin4  11     // IN4 on the ULN2003 driver
 #define Servopin 7        // Pin 7 PWM on Arduino
-#define  STROBE_TM1 4     // STRB0 of board1 is om pin 4
-#define  STROBE_TM2 5     // STRB1 of board 2 is on pin 5
-#define  STROBE_TM3 12     // STRB1 of board 3 is on pin 12
-
-#define  CLOCK_TM 2       // CLK of all boards is on pin 2
-#define  DIO_TM 3         // DIO of all boards is on pin 3
-#define TM_BRT 0x02       // set the brightness of board (0-7)
 
 #define STEPS (315*3) // 945
 // the x27 stepper lists 1/3deg per step.=3 steps per deg = 3*360 steps per full rotation = 1080 steps per revolution.
@@ -24,86 +14,6 @@
 const int X27StepsPerRevolution = 1080;
 const int maxSteps = 945; // X27 can only turn 315°
 
-TM1638 tm1(DIO_TM, CLOCK_TM, STROBE_TM1);
-TM1638 tm2(DIO_TM, CLOCK_TM, STROBE_TM2);
-TM1638 tm3(DIO_TM, CLOCK_TM, STROBE_TM3);
-//word leds [17] = {0, 256, 768, 1792, 3840, 7936, 16128, 32512, 65280, 1, 3, 7, 15, 31, 63, 127, 255};
-//https://tronixstuff.com/2012/03/11/arduino-and-tm1638-led-display-modules/
-
-/*Set the display (segments and LEDs) active or off and intensity (range from 0-7).
-  setupDisplay(boolean active, byte intensity)
-
-  Set a single display at pos (starting at 0) to a digit (left to right)
-  setDisplayDigit(byte digit, byte pos, boolean dot, const byte numberFont[] = NUMBER_FONT)
-
-  Clear  a single display at pos (starting at 0, left to right)
-  clearDisplayDigit(byte pos, boolean dot)
-
-  Set the display to the values (left to right)
-  setDisplay(const byte values[], unsigned int length = 8)
-
-  Clear the display
-  clearDisplay()
-
-  Set the display to the string (defaults to built in font)
-  setDisplayToString(const char* string, const word dots = 0, const byte pos = 0,
-    const byte font[] = FONT_DEFAULT)
-
-  Instantiate a tm1638 module specifying the display state, the starting intensity (0-7) data, clock and stobe pins.
-    TM1638(byte dataPin, byte clockPin, byte strobePin, boolean activateDisplay = true, byte intensity = 7)
-
-    /** Set the display to a unsigned hexadecimal number (with or without leading zeros)
-    setDisplayToHexNumber(unsigned long number, byte dots, boolean leadingZeros = true,
-    const byte numberFont[] = NUMBER_FONT)
-
-    /** Set the display to a unsigned decimal number (with or without leading zeros)
-    setDisplayToDecNumber(unsigned long number, byte dots, boolean leadingZeros = true,
-    const byte numberFont[] = NUMBER_FONT)
-
-    /** Set the display to a signed decimal number (with or without leading zeros)
-   setDisplayToSignedDecNumber(signed long number, byte dots, boolean leadingZeros = true,
-    const byte numberFont[] = NUMBER_FONT)
-
-    /** Set the display to a unsigned binary number
-    setDisplayToBinNumber(byte number, byte dots,
-    const byte numberFont[] = NUMBER_FONT)
-
-    /** Set the LED at pos to color (TM1638_COLOR_RED, TM1638_COLOR_GREEN or both)
-   setLED(byte color, byte pos)
-    /** Set the LEDs. MSB byte for the green LEDs, LSB for the red LEDs
-    setLEDs(word led)
-
-    /** Returns the pressed buttons as a bit set (left to right)
-    virtual byte getButtons()
-*/
-//LiquidCrystal_I2C lcd(0x27, 20, 4);
-LiquidCrystal_I2C lcd(0x3F, 16, 2);  // define object of LCD
-/* LCD
-   Stel hier in welke chip en foromaat LCD je hebt
-   Gebruik 0x27 als je chip PCF8574 hebt van NXP
-   Gebruik 0x3F als je chip PCF8574A hebt van Ti (Texas Instruments)
-   De laatste twee getallen geven het formaat van je LCD aan
-   bijvoorbeeld 20x4 of 16x2
-
- *  *
-   PIN AANSLUITINGEN
-
-   SDA is serial data
-   SCL is serial clock
-
-   GND --> GND wit
-   VCC --> 5V  grijs
-   SDA --> A4  rood
-   SCL --> A5  blauw
-
-
-   I2C aansluitingen per Arduino:
-   Uno, Ethernet    A4 (SDA), A5 (SCL)
-   Mega2560         20 (SDA), 21 (SCL)
-   Leonardo          2 (SDA),  3 (SCL)
-   Due              20 (SDA), 21 (SCL) of SDA1, SCL1
-
-*/
 Servo MyServo; // define an object of servomotor
 Servo MyServo2; // define an object of servomotor
 // *********************************************
@@ -125,21 +35,8 @@ int StepVario = 20; // 1 ms/s = 20 steps on the servo --> 0.1 m/s = 2 steps on t
 
 Stepper ACSpeedStepper(X27StepsPerRevolution, 8, 9, 10, 11);
   
-byte buttons1 = 0;
-byte buttons2 = 0;
-//byte buttons3=0;
-byte oldbuttons1 = 0;
-byte oldbuttons2 = 0;
-//byte oldbuttons3=0;
-byte page1 = 1;
-byte page2 = 1;
-//byte page3=1;
-
-bool changed;
 
 bool homing = false;
-bool lcdon = false;
-bool TM1638on = true;
 bool ServoOn = true;
 
 int altl, spdl, hdgl, bnkl, pitl, varrl, varel, varil, gfol, yawl;
@@ -164,46 +61,6 @@ void setup()
   ACSpeedStepper.setSpeed(25);
   ACSpeedStepper.step (-1*maxsteps);
 
-  if (lcdon)
-  {
-    lcd.init();                  // initialiseer het LCD scherm
-    lcd.backlight();             // zet de backlight aan
-    lcd.clear();                 // maak leeg
-    lcd.setCursor(0, 0);         // cursor linksboven
-  }
-  else
-  { // lcd is off
-    lcd.clear();
-    lcd.noBacklight();
-  }
-
-  if (TM1638on)
-  {
-    tm1.setupDisplay(true, 1);
-    tm2.setupDisplay(true, 1);
-    tm3.setupDisplay(true, 1);
-
-    tm1.clearDisplay();
-    tm2.clearDisplay();
-    tm3.clearDisplay();
-
-    tm1.setLEDs(0xFF00);
-    tm2.setLEDs(0xFF00);
-    tm3.setLEDs(0xFF00);
-    byte values[] = { 99, 99, 99, 99, 99, 99, 99, 99 };
-    tm1.setDisplay(values);
-    tm2.setDisplay(values);
-    tm3.setDisplay(values);
-  }
-  else // its off
-  {
-    tm1.clearDisplay(); // clears the display, ot the leds
-    tm2.clearDisplay();
-    tm3.clearDisplay();
-    tm1.setupDisplay(false, 2);
-    tm2.setupDisplay(false, 2);
-    tm3.setupDisplay(false, 2);
-  }
 
   Serial.begin(19200);
 }
@@ -253,27 +110,6 @@ void loop()
 
         if (vari < -5) vari = -4.9;
         if (vari > 5) vari = 4.9;
-
-        //debug info
-        if (lcdon)
-        {
-          lcd.setCursor(0, 0);
-          lcd.print("var:" + String(varr)); //or (String(val, DEC)
-          lcd.setCursor(0, 1);
-          lcd.print("var i:" + String(vari)); // for floats.
-        }
-
-        if (TM1638on)
-        {
-          tm1.setDisplayToSignedDecNumber(alt, 0, false);
-          tm2.setDisplayToSignedDecNumber(spd, 0, false);
-          tm3.setDisplayToSignedDecNumber(spdval, 0, false);
-          tm1.setLEDs(0);
-          tm3.setLEDs(0);
-
-          // Set the Yawstring
-          YawLeds (tm2, yaw);
-        }
       }
     }
   }
@@ -302,78 +138,5 @@ void loop()
    MyServo.write(varr * StepVario + 100); //0.1 m/s *20 +100 = 102
    MyServo2.write(vari * StepVario + 100); //
   }
-  /*
-    if (TM1638on)
-    {
-
-     buttons2 = tm2.getButtons();
-     if (buttons2 != 0) //buttons are pressed sir
-     {
-       if (buttons2 != oldbuttons2) //and it is not the same one
-       {
-         oldbuttons2 = buttons2;
-         page2 = buttons2;
-         tm1.clearDisplay();
-         tm2.clearDisplay();
-         tm1.setLEDs(0);
-         tm2.setLEDs(0);
-       }
-     }
-
-     switch (page2)
-     {
-       case 1: //left most button
-         {
-           TMDint(tm1, "spd", spd);
-           break;
-         }
-       case 2: // button 2
-         {
-           TMDint(tm2, "alt", alt);
-           break;
-         }
-       case 4: // button 3
-         {
-           TMDint(tm2, "bank", bnk);
-           break;
-         }
-       case 8: // button 4
-         {
-           TMDint(tm2, "pit", pit);
-           break;
-         }
-       case 16: // button 5
-         {
-           TMDint(tm2, "hdg", hdg);
-           break;
-         }
-       case 32: // button 6
-         {
-           TMDfloat(tm1, "G", gfo);
-           break;
-         }
-     }
-
-    }
-  */
-
+ 
 }//end loop
-
-void YawLeds(TM1638 m, double a)
-{
-  // Set the Yawstring
-  //*****************
-  //https://www.mathsisfun.com/binary-decimal-hexadecimal-converter.html
-
-  if (a == 0) m.setLEDs(0);
-  //Yaw Right:
-  if (a > 0.0 && a < 2.0) m.setLEDs(4096);
-  if (a >= 2.0 && a < 4.0) m.setLEDs(8192);
-  if (a >= 4.0 && a < 6.0) m.setLEDs(16384);
-  if (a >= 8.0) m.setLEDs(32768);
-  //Yaw Left:
-  if (a <= -8.0) m.setLEDs(256);
-  if (a >= -6.0 && a < -4.0) m.setLEDs(512);
-  if (a >= -4.0 && a < -2.0) m.setLEDs(1024);
-  if (a >= -2.0 && a < 0.0) m.setLEDs(2048);
-}
